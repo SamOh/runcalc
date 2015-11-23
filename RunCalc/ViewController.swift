88a80389a06bcc82ca735b3b3d738945929a8f38
Sammy Oh <soh02@college.harvard.edu>
2015-11-23 12:31:02 -0500

    Initial Commit

---
+++ RunCalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RunCalc
+//
+//  Created by Samuel Oh on 11/23/15.
+//  Copyright (c) 2015 Samuel Oh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
